0342d6ccbee199ac17964fd427b55d91a00e0c4f
Jean-Claude <jeandev84@yahoo.com>
2021-11-25 04:00:53 +0300

    create skeleton class application

---
+++ framework/src/Foundation/Application.php
@@ -0,0 +1,16 @@
+<?php
+namespace Jan\Foundation;
+
+
+use Jan\Component\Container\Container;
+
+
+/**
+ * @see Application
+ *
+ * @package Jan\Foundation
+*/
+class Application extends Container
+{
+
+}
